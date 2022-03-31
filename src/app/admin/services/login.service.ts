c37ec2bdcfa47c0c81e5c9b423d93b036c5e79de
Michael Irigoyen <michael@irigoyen.dev>
2022-03-30 22:36:51 -0500

    Fix auth check

--- src/app/admin/services/login.service.ts
+++ src/app/admin/services/login.service.ts
@@ -1,6 +1,5 @@
 import { Injectable }     from '@angular/core';
-import { Observable }     from 'rxjs/Rx';
-import { Router, Route, ActivatedRoute } from '@angular/router';
+import { Router } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 import { PromiseCache } from 'app/shared/promiseCache.decorator';
 import { PromiseCacheService } from 'app/shared/promiseCache.service';
@@ -8,16 +7,11 @@ import { User } from 'app/shared/models/user.model';
 
 @Injectable()
 export class LoginService {
-
-  private url: string;
-
   constructor (
     private http: HttpClient,
     private router: Router,
-    private route: ActivatedRoute,
     private promiseCacheService: PromiseCacheService
   ) {
-    this.url = route.snapshot.url.map(m => m.path).join('/');
   }
 
   login (user: string, pass: string): Promise<boolean> {
@@ -36,10 +30,7 @@ export class LoginService {
     return this.http.get<boolean>('/api/admin')
         .toPromise()
         .then(isAuthed => {
-          if (redirect
-            && (this.url != 'admin'
-            && this.url.match(/^admin/) != null)
-            && !(isAuthed)) {
+          if (redirect && this.router.url.startsWith('/admin') != null && !isAuthed) {
             this.router.navigateByUrl('/admin');
           }
           return isAuthed;
@@ -63,5 +54,4 @@ export class LoginService {
           return null;
         });
   }
-
 }
