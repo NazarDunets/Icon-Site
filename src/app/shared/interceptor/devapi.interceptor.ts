406520c00357cc50ecd77cd8a5dd9887ab144ef3
Michael Irigoyen <michael@irigoyen.dev>
2022-04-06 15:36:32 -0500

    Add a devapi environment to force the app to use the local dev api

---
+++ src/app/shared/interceptor/devapi.interceptor.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+
+@Injectable()
+export class MockInterceptor implements HttpInterceptor {
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    console.log({
+      url: req.url,
+      urlWithParams: req.urlWithParams,
+      body: req.body
+    });
+    const mockReq = req.clone({
+      url: `http://localhost:9080${req.url}`,
+      method: 'GET',
+      params: new HttpParams()
+    });
+    return next.handle(mockReq);
+  }
+}
